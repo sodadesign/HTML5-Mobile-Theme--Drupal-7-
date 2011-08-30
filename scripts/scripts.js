0c91f9da3d9a3342fcb242ee23053dfdda39a841
James O'Toole <goaway@dearoccupant.com>
2011-08-30 11:09:16 -0700

    Minor Fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,6 @@
 (function ($) {
   // All your code here  
-  $(document).ready(function() {
-    
-    // Expand Panel
-  	$("#open").click(function(){
-  		$(".block-webform .block-inner").slideDown("fast");
-
-  	});	
-
-  	// Collapse Panel
-  	$("#close").click(function(){
-  		$(".block-webform .block-inner").slideUp("fast");	
-  	});		
-
-  	// Switch buttons from "Log In | Register" to "Close Panel" on click
-  	$("#toggle a").click(function () {
-  		$("#toggle a").toggle();
-  	});
+  
   	
 // end document.ready
   });
